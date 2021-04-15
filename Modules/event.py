45a37cd95c0e685911b8c8be12a3624ea517f86e
GiulianoDerosas <giulianoderosas@gmail.com>
2021-04-15 16:54:59 +0400

    Initialised class, and created our first event

---
+++ Modules/event.py
@@ -0,0 +1,8 @@
+class Event():
+
+    def __init__(self, date, event_name, num_guest, room_loc, description):
+        self.date = date
+        self.event_name = event_name
+        self.num_guest = num_guest
+        self.room_loc = room_loc
+        self.description = description
---
+++ Modules/event_list.py
@@ -0,0 +1,3 @@
+from modules.event import Event
+
+event_1 = Event("14-04-2021", "Danny's Birthday", 50, "Grand Ballroom", "An elegant birthday bash!")
